735e5940efdd23c888e9a2168f82044afe5a6ea1
TUdayKiranReddy <53467166+TUdayKiranReddy@users.noreply.github.com>
2022-03-14 11:53:04 +0530

    Add files via upload

--- ESC_ESP32/src/main.cpp
+++ ESC_ESP32/src/main.cpp
@@ -1,22 +1,62 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <DabbleESP32.h>
 #include <ESP32Servo.h>
 #include "ESC.h"
 
-ESC esc(ESC::MODE_FORWARD_BACKWARD);
+ESC L_motor;
+ESC R_motor;
 
-void setup() 
-{
+void _setup() {
+  L_motor.attach(14);
+  R_motor.attach(15);
+
+  Dabble.begin("UDAY_UGV");
+}
+
+void _loop() {
+  Dabble.processInput();
+}
+
+void setup() {
   Serial.begin(9600);
-  esc.attach(14);
-} 
- 
-void loop() 
-{ 
-    esc.setSpeed(0);
-    esc.setDirection(ESC::FORWARD);
-    esc.setSpeed(30);
-    delay(1000);
-    esc.setSpeed(0);
-    esc.setDirection(ESC::BACKWARD);
-    esc.setSpeed(30);
-    delay(1000);
-} 
+  _setup();
+}
+
+void loop() {
+  _loop();
+  if (GamePad.isPressed(0)) {
+    Serial.println("FORWARD");
+    L_motor.setDirection(ESC::FORWARD);
+    L_motor.setSpeed(30);
+
+    R_motor.setDirection(ESC::FORWARD);
+    R_motor.setSpeed(30);
+    
+  }
+  else if (GamePad.isPressed(1)){
+    Serial.println("REVERSE");
+    L_motor.setDirection(ESC::FORWARD);
+    L_motor.setSpeed(30);
+
+    R_motor.setDirection(ESC::FORWARD);
+    R_motor.setSpeed(0);
+  }
+  else if (GamePad.isPressed(3)) {
+   Serial.println("RIGHT");
+  
+    L_motor.setDirection(ESC::FORWARD);
+    L_motor.setSpeed(30);
+
+    R_motor.setDirection(ESC::FORWARD);
+    R_motor.setSpeed(0);
+  }
+  else if (GamePad.isPressed(2)) {
+    Serial.println("LEFT");
+    L_motor.setDirection(ESC::FORWARD);
+    L_motor.setSpeed(30);
+
+    R_motor.setDirection(ESC::FORWARD);
+    R_motor.setSpeed(30);
+  }
+}
